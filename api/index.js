e8ff89b479a5c69eb4fd9b56213d04e09137db01
Babu S G <bsg97315@gmail.com>
2023-04-30 01:13:18 +0530

    no idea what happened

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -13,6 +15,15 @@ mongoose.connect(process.env.MONGO_URL,
     console.log("Connected to MongoDB")
 });
 
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+
 app.listen(8800,()=>{
-    console.log("Backend server is running")
-})
+    console.log("Backend Server is runnin");
+});
